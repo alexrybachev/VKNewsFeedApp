5c79ff4855a3122c6c13d80b3ae9daa3a3910d39
Alex Rybachev <rybachev.as@gmail.com>
2023-01-07 00:12:23 +0400

    UICollectionViewCustomLayout Part 2

--- VKNewsFeedApp/NewsFeed/GalleryView/GalleryCollectionView.swift
+++ VKNewsFeedApp/NewsFeed/GalleryView/GalleryCollectionView.swift
@@ -13,17 +13,22 @@ class GalleryCollectionView: UICollectionView {
     var photos = [FeedCellPhotoAttachmentViewModel]()
     
     init() {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        let rowLayout = Rowlayout()
+        let rowLayout = RowLayout()
         super.init(frame: .zero, collectionViewLayout: rowLayout)
         
         delegate = self
         dataSource = self
         
-        backgroundColor = .blue
+        backgroundColor = UIColor.white
+        
+        showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
         
         register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: GalleryCollectionViewCell.reuseId)
+        
+        if let rowLayout = collectionViewLayout as? RowLayout {
+            rowLayout.delegate = self
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -32,6 +37,7 @@ class GalleryCollectionView: UICollectionView {
     
     func set(photos: [FeedCellPhotoAttachmentViewModel]) {
         self.photos = photos
+        contentOffset = CGPoint.zero
         reloadData()
     }
 }
@@ -46,11 +52,8 @@ extension GalleryCollectionView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = dequeueReusableCell(withReuseIdentifier: GalleryCollectionViewCell.reuseId, for: indexPath) as! GalleryCollectionViewCell
         cell.set(imageURL: photos[indexPath.row].photoURLString)
-        
         return cell
     }
-    
-    
 }
 
 // MARK: - UICollectionViewDelegate
@@ -60,8 +63,20 @@ extension GalleryCollectionView: UICollectionViewDelegate {
 
 // MARK: - UICollectionViewDelegateFlowLayout
 //extension GalleryCollectionView: UICollectionViewDelegateFlowLayout {
-//    
+//
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 //        return CGSize(width: frame.width, height: frame.height)
 //    }
 //}
+
+// MARK: - RowLayoutDelegate
+extension GalleryCollectionView: RowLayoutDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, photoAtIndexPath indexPath: IndexPath) -> CGSize {
+        let width = photos[indexPath.row].width
+        let height = photos[indexPath.row].height
+        return CGSize(width: width, height: height)
+    }
+    
+    
+}
--- VKNewsFeedApp/NewsFeed/GalleryView/GalleryCollectionViewCell.swift
+++ VKNewsFeedApp/NewsFeed/GalleryView/GalleryCollectionViewCell.swift
@@ -15,20 +15,29 @@ class GalleryCollectionViewCell: UICollectionViewCell {
     let myImageView: WebImageView = {
        let imageView = WebImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = .systemPink
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        
         addSubview(myImageView)
         myImageView.fillSuperview()
     }
     
     override func prepareForReuse() {
         myImageView.image = nil
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        myImageView.layer.masksToBounds = true
+        myImageView.layer.cornerRadius = 10
+        self.layer.shadowRadius = 3
+        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize(width: 2.5, height: 4)
     }
     
     required init?(coder: NSCoder) {
